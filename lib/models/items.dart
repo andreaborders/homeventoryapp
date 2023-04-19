1146315df059fffa6088343bd9d00d87390e5474
andreaborders <84049664+andreaborders@users.noreply.github.com>
2023-04-18 19:16:29 -0500

    Delete items.dart

--- lib/models/items.dart
+++
@@ -1,7 +0,0 @@
-class RoomItem {
-  final String name;
-  final String imageAsset;
-  final String description;
-
-  RoomItem({required this.name, required this.imageAsset, required this.description});
-}
